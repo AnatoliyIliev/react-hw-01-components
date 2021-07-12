8e41eee21dbb38029f5763d5021278470cbd1bd7
Anatoliy Iliev <vekok12@gmail.com>
2021-07-12 23:39:49 +0300

    hw1-day2

--- src/components/friend-list/FriendList.js
+++ src/components/friend-list/FriendList.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import styles from './FriendList.module.scss';
 
 const FriendList = ({ friends }) => (
-  <ul class="friend-list">
+  <ul className={styles.friend_list}>
     {friends.map(({ avatar, name, isOnline, id }) => (
-      <li class="item" key={id}>
+      <li className={styles.item} key={id}>
         <span class="status">{isOnline ? 'green' : 'red'}</span>
         <img class="avatar" width="48" src={avatar} alt={name}></img>
         <p class="name">{name}</p>
