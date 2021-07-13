3f9e3ec54673ff2996b22722f469b03359e54ba7
Anatoliy Iliev <vekok12@gmail.com>
2021-07-13 07:47:52 +0300

    hw1-day3

--- src/components/friend-list/FriendList.js
+++ src/components/friend-list/FriendList.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+
 import styles from './FriendList.module.scss';
 
 const FriendList = ({ friends }) => (
--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Statistics = ({ title, stats }) => (
   <section class="statistics">
-    {title ? <h2 class="title">Upload stats</h2> : null}
+    {title && <h2 class="title">Upload stats</h2>}
     <ul class="stat-list">
       {stats.map(({ id, label, percentage }) => (
         <li class="item" key={id}>
