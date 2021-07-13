02b90a1f5fa8083d0c07c2bc2a0e14eb0f99caeb
Anatoliy Iliev <vekok12@gmail.com>
2021-07-14 00:13:49 +0300

    day4

--- src/components/friend-list/FriendList.js
+++ src/components/friend-list/FriendList.js
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
-
 import styles from './FriendList.module.scss';
 
 const FriendList = ({ friends }) => (
   <ul className={styles.friend_list}>
     {friends.map(({ avatar, name, isOnline, id }) => (
       <li className={styles.item} key={id}>
-        <span class="status">{isOnline ? 'green' : 'red'}</span>
-        <img class="avatar" width="48" src={avatar} alt={name}></img>
-        <p class="name">{name}</p>
+        <span
+          className={styles.status}
+          style={{ background: isOnline ? 'green' : 'red' }}
+        ></span>
+        <img className={styles.avatar} src={avatar} alt={name}></img>
+        <p className={styles.name}>{name}</p>
       </li>
     ))}
   </ul>
--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,13 +1,27 @@
 import PropTypes from 'prop-types';
+import styles from './Statistics.module.scss';
+
+const colors = [
+  { color: '#FFFFFF' },
+  { color: '#2196F3' },
+  { color: '#4CAF50' },
+  { color: '#FF9800' },
+  { color: '#009688' },
+  { color: '#795548' },
+];
 
 const Statistics = ({ title, stats }) => (
-  <section class="statistics">
-    {title && <h2 class="title">Upload stats</h2>}
-    <ul class="stat-list">
+  <section className={styles.statistics}>
+    {title && <h2 className={styles.title}>Upload stats</h2>}
+    <ul className={styles.stat_list}>
       {stats.map(({ id, label, percentage }) => (
-        <li class="item" key={id}>
-          <span class="label">{label}</span>
-          <span class="percentage">{percentage}%</span>
+        <li
+          className={styles.item}
+          style={{ background: colors.map(({ color }) => color) }}
+          key={id}
+        >
+          <span className={styles.label}>{label}</span>
+          <span className={styles.percentage}>{percentage}%</span>
         </li>
       ))}
     </ul>
